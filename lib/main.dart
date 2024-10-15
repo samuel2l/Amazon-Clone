31b7f04e80b80f1f1482dd2413271d3455d187dd
Samuel Adams <sama29571@gmail.com>
2024-10-15 14:39:07 +0000

    add constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UrbanNest',
       theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black)
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
