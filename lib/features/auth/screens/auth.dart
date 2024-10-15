027a61dd2514587e054859f0279d897afd406540
Samuel Adams <sama29571@gmail.com>
2024-10-15 19:26:27 +0000

    add auth form fields

--- lib/features/auth/screens/auth.dart
+++ lib/features/auth/screens/auth.dart
@@ -1,7 +1,8 @@
+import 'package:amazon/constants.dart';
 import 'package:flutter/material.dart';
 
 class Auth extends StatefulWidget {
-  static const String routeName='auth'; 
+  static const String routeName = 'auth';
   const Auth({super.key});
 
   @override
@@ -9,8 +10,77 @@ class Auth extends StatefulWidget {
 }
 
 class _AuthState extends State<Auth> {
+  String auth = 'signUp';
+  final signUpKey = GlobalKey<FormState>();
+  final logInKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: greyBackgroundCOlor,
+      body: SafeArea(
+          child: Column(
+        children: [
+          ListTile(
+            title: const Text('Create account'),
+            leading: Radio(
+                // the value and group value have to be of same type and this also affects the value parameer the onchanged will take
+                activeColor: secondaryColor,
+                value: 'signUp',
+                groupValue: auth,
+                onChanged: (value) => setState(() {
+                      auth = value!;
+                    })),
+          ),
+          auth == 'signUp'
+              ? Form(
+                  child: Column(
+                  children: [
+                    TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black26),
+                        ),
+                      ),
+                      validator: (value) {},
+                    ),
+                                        TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black26),
+                        ),
+                      ),
+                      validator: (value) {},
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black26),
+                        ),
+                      ),
+                      validator: (value) {},
+                    ),
+
+                  ],
+                ))
+              : const SizedBox.shrink(),
+          ListTile(
+            title: const Text('Log in'),
+            leading: Radio(
+                activeColor: secondaryColor,
+                value: 'signIn',
+                groupValue: auth,
+                onChanged: (value) => setState(() {
+                      auth = value!;
+                    })),
+          ),
+        ],
+      )),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:amazon/constants.dart';
+import 'package:amazon/features/auth/screens/auth.dart';
+import 'package:amazon/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,8 +9,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,18 +16,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: backgroundColor,
         appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black)
-        ),
+            elevation: 0, iconTheme: IconThemeData(color: Colors.black)),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      onGenerateRoute: (settings) {
-        
-      },
-      home: const Text('hey there'),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const Auth(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
